80fb63785601ba5f77e9ac97d2597a5d04c02ffa
Maeril <Maeril@Avesta.local>
2019-02-02 15:31:45 +0100

    fix path to libft in fillit.h :3c -Mae

--- fillit/fillit.h
+++ fillit/fillit.h
@@ -6,14 +6,14 @@
 /*   By: myener <myener@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 15:45:54 by mribouch          #+#    #+#             */
-/*   Updated: 2019/01/31 15:25:47 by myener           ###   ########.fr       */
+/*   Updated: 2019/02/02 15:31:19 by myener           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 
 # define FILLIT_H
-# include "libft.h"
+# include "libft/libft.h"
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
